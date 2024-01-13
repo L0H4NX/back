8bcf70dd285f646a120582c79f2c8814e5ba46a3
L0H4NX <lsteferson83@gmail.com>
2024-01-13 11:16:56 -0300

    Feat - create table controller user and exercise note

---
+++ src/main/java/com/zoeAcademy/models/exercise/UserExercise.java
@@ -0,0 +1,32 @@
+package com.zoeAcademy.models.exercise;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.zoeAcademy.models.User;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class UserExercise {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "exercise_id", nullable = false)
+    private Exercise exercise;
+
+    private Long note;
+
+}
